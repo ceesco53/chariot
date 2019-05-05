4c327ccc58ab60bc9bc2384dde7ab76d90ddbb70
John Nelson <ceesco53@gmail.com>
2019-05-05 09:21:20 -0400

    update config env in CF Service Provider

--- app/Providers/CloudFoundryServiceProvider.php
+++ app/Providers/CloudFoundryServiceProvider.php
@@ -22,16 +22,16 @@ class CloudFoundryServiceProvider extends ServiceProvider
             $redisConfig = $config['rediscloud'][0]['credentials'];
 
             // Set the MySQL config.
-            Config::set('database.connections.mysql.host', $mysqlConfig['hostname']);
-            Config::set('database.connections.mysql.port', $mysqlConfig['port']);
-            Config::set('database.connections.mysql.database', $mysqlConfig['name']);
-            Config::set('database.connections.mysql.username', $mysqlConfig['username']);
-            Config::set('database.connections.mysql.password', $mysqlConfig['password']);
+            config(['database.connections.mysql.host' => $mysqlConfig['hostname']]);
+            config(['database.connections.mysql.port' => $mysqlConfig['port']]);
+            config(['database.connections.mysql.database' => $mysqlConfig['name']]);
+            config(['database.connections.mysql.username' => $mysqlConfig['username']]);
+            config(['database.connections.mysql.password' => $mysqlConfig['password']]);
 
             // Set the Redis config.
-            Config::set('database.redis.clusters.default.0.host', $redisConfig['hostname']);
-            Config::set('database.redis.clusters.default.0.password', $redisConfig['password']);
-            Config::set('database.redis.clusters.default.0.port', $redisConfig['port']);
+            config(['database.redis.clusters.default.0.host' => $redisConfig['hostname']]);
+            config(['database.redis.clusters.default.0.password' => $redisConfig['password']]);
+            config(['database.redis.clusters.default.0.port' => $redisConfig['port']]);
         }
     }
 }
