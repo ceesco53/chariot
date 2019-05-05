6a05fc4584439ff80f9a511b78ffa50e4fbbe60f
John Nelson <ceesco53@gmail.com>
2019-05-04 23:05:26 -0400

    update deploy script with cf login correction

--- deploy.sh
+++ deploy.sh
@@ -6,11 +6,8 @@ SECRETS_FILE="secrets.travis.yml"
 # Set missing variables.
 php artisan cf:set-secrets --path=$SECRETS_FILE
 
-# Connect to the Cloud Foundry API.
-cf api $CF_API
-
 # Login to Cloud Foundry.
-cf login -u $CF_USERNAME -p $CF_PASSWORD -o $CF_ORGANISATION -s $CF_SPACE
+cf login -a $CF_API -u $CF_USERNAME -p $CF_PASSWORD -o $CF_ORGANISATION -s $CF_SPACE
 
 # Deploy
 cf push --vars-file $SECRETS_FILE
